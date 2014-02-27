867601b3f8e225a90b1d9da69e9129cd5e5528a0
chris-maroudas <mallibu97@gmail.com>
2014-02-27 22:51:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheNoirBox::Application.config.secret_token = 'b8d4b7674657db5b2a2371a72d87106ea9a84dbbe4947e6ca1cdb19781b17cb17259fd1885e3a25774f416e41b6f117a5bee6a1ec2b2d3ccf60cc10c012f4d5c'
