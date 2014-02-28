f04a4fcde2247ca81d7cbd9e86851ec93e81c2cd
chris-maroudas <mallibu97@gmail.com>
2014-02-28 04:13:03 +0200

    Added tags functionality to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,32 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :taglist
+
+  has_and_belongs_to_many :tags
+  validates :title, presence: true, uniqueness: true, length: { maximum: 120 }
+  validates :content, presence: true, length: { maximum: 12000 }
+
+  def taglist
+    tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def taglist=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq # Create an array with the unique, normalized tags received
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by_name(name) } # Create the tags, if they don't exist
+    self.tags = new_or_found_tags # and associate them with this article
+  end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140228021133) do
+ActiveRecord::Schema.define(:version => 20140228021136) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -29,9 +29,31 @@ ActiveRecord::Schema.define(:version => 20140228021133) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
 
+  create_table "friendly_id_slugs", :force => true do |t|
+    t.string   "slug",                         :null => false
+    t.integer  "sluggable_id",                 :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
+  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts_tags", :id => false, :force => true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
